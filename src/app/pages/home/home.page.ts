2808288758b449fde77e9f01f5c00758524e2325
Abdelraouf Hecham <hecham.abdelraouf@gmail.com>
2022-05-29 23:52:56 +0200

    chore: added some skills photos

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -71,6 +71,155 @@ export class HomePage implements OnInit, AfterViewInit, OnDestroy {
     },
   ];
 
+  public timeline = [
+    {
+      start: 2019,
+      end: 2022,
+      title: 'Data Engineer',
+      company: 'Business & Decision',
+      location: 'Montpellier, France',
+      description: '',
+      items: ['potato', 'boy'],
+    },
+    {
+      start: 2018,
+      end: 2019,
+      title: 'Research Engineer',
+      company: 'University of Montpellier - LIRMM',
+      location: 'Montpellier, France',
+      description:
+        'Formalized mathematical models and built a platform for real-time collective decision making based on potentially conflicting shared knowledge.',
+      items: [
+        'Implemented modular architecture across the entire stack while overseeing deployment, documentation, and deliverable reports.',
+        'Implemented Spring REST API along with real-time collaboration using websockets.',
+        'Implemented Angular Front-end with graph visualization and interaction featurs.',
+      ],
+      skills: [
+        {
+          src: 'logo-ionic',
+          title: 'Ionic',
+        },
+        {
+          src: 'logo-tux',
+          title: 'Linux',
+        },
+      ],
+    },
+  ];
+
+  public skills = {
+    front: [
+      {
+        src: './assets/img/ionic.png',
+        title: 'Ionic',
+      },
+      {
+        src: './assets/img/angular.png',
+        title: 'Angular (Typescript)',
+      },
+      {
+        src: './assets/img/react.png',
+        title: 'React (Javascript)',
+      },
+      {
+        src: './assets/img/capacitor.png',
+        title: 'Capacitor (Android)',
+      },
+    ],
+    back: [
+      {
+        src: './assets/img/nodejs.png',
+        title: 'Nodejs',
+      },
+      {
+        src: './assets/img/flask.png',
+        title: 'Flask (Python)',
+      },
+      {
+        src: './assets/img/spring.png',
+        title: 'Spring (Java)',
+      },
+    ],
+    smart: [
+      {
+        src: './assets/img/solidity.png',
+        title: 'Solidity',
+      },
+      {
+        src: './assets/img/ethersjs.png',
+        title: 'Ethers.js',
+      },
+    ],
+    data: [
+      {
+        src: './assets/img/mogodb.png',
+        title: 'Mongodb',
+      },
+      {
+        src: './assets/img/postgres.png',
+        title: 'Postgres',
+      },
+      {
+        src: './assets/img/oracle.png',
+        title: 'Oracle',
+      },
+      {
+        src: './assets/img/tensorflow.png',
+        title: 'TensorFlow',
+      },
+      {
+        src: './assets/img/spark.png',
+        title: 'Apache Spark',
+      },
+      {
+        src: './assets/img/tableau.png',
+        title: 'Tableau',
+      },
+      {
+        src: './assets/img/talend.png',
+        title: 'Talend',
+      },
+      {
+        src: './assets/img/qlik.png',
+        title: 'Qlik Sense',
+      },
+      {
+        src: './assets/img/salesforce.png',
+        title: 'Salesforce',
+      },
+    ],
+    ops: [
+      {
+        src: './assets/img/aws.png',
+        title: 'AWS',
+      },
+      {
+        src: './assets/img/azure.png',
+        title: 'Azure',
+      },
+      {
+        src: './assets/img/git.png',
+        title: 'Git',
+      },
+      {
+        src: './assets/img/npm.png',
+        title: 'npm',
+      },
+      {
+        src: './assets/img/docker.png',
+        title: 'Docker',
+      },
+      {
+        src: './assets/img/maven.png',
+        title: 'Maven',
+      },
+      {
+        src: './assets/img/gradle.png',
+        title: 'Gradle',
+      },
+    ],
+  };
+
   smallSideMenu: boolean = false;
   menuWidth: string = '370px';
 
